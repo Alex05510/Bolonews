f5aa608f7b1b307239a644d6ad317612dac5c653
Alex05510 <alexandre.audier@icloud.com>
2025-07-31 17:46:20 +0200

    mise en place de la gestion de connexion/deconnexion

---
+++ src/Form/LoginFormType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class LoginFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Password',
+            ])
+            ->add('_remember_me', CheckboxType::class, [
+                'label'    => 'Remember me',
+                'required' => false,
+            ]);
+    }
+}
